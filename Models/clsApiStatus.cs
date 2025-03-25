f332802979b55e37cedb8accdbd7949626e5b8d9
Allmidon <155588419+Allmidon@users.noreply.github.com>
2025-03-24 22:09:36 -0600

    Add files via upload

---
+++ Models/clsApiStatus.cs
@@ -0,0 +1,19 @@
+﻿//EXAMEN SEGUNDO SEGUIMIENTO
+//Taller Avanzado de Desarrollo de Software
+//Integrantes:
+//Moedano Alcántara María Fernanda
+//Ortiz Velazquez Gabriela Paola 
+//Soto Cadena Alan
+
+using Newtonsoft.Json.Linq;
+
+namespace apiRESTCine.Models
+{
+    public class clsApiStatus
+    {
+        public bool statusExec { get; set; }
+        public int ban { get; set; }
+        public string msg { get; set; }
+        public object datos { get; set; }
+    }
+}
